3ea001080c59ad173b0602066afc426e91a9f9b4
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-11-03 00:55:56 +0100

    exercise completed and all tests passed

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,42 @@
 const Person = require('./person');
 
 // Your code here
+class Student extends Person {
+  constructor(firstName, lastName, major, GPA) {
+    super(firstName, lastName);
+
+    this.major = major;
+    this.GPA = GPA;
+  }
+
+  static compareGPA(student1, student2) {
+
+    if (student1.GPA === student2.GPA) {
+      return "Both students have the same GPA";
+    }
+
+    const student = Student.maxGPA(student1, student2);
+
+    return `${student.firstName} ${student.lastName} has the higher GPA.`;
+  }
+
+  static maxGPA(student1, student2) {
+    if (student1.GPA > student2.GPA) {
+      return student1;
+    }
+
+    return student2;
+  }
+}
+
+const std1 = new Student("Kunle", "Ade", "Physics", 2.9);
+const std2 = new Student("Tope", "Kola", "Chemistry", 2.9);
+
+const highGrade = Student.compareGPA(std1, std2);
+
+console.log(highGrade);
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -2,9 +2,34 @@ const Person = require('./person');
 
 // Your code here
 
+class Teacher extends Person {
+  constructor(firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teachers) {
+    let combined = 0;
+
+    teachers.forEach(teacher => {
+      combined += teacher.yearsOfExperience;
+    });
+
+    return combined;
+  }
+}
+
+const teacher1 = new Teacher("Hannah", "Florence", "Sciences", 10);
+const teacher2 = new Teacher("Sandra", "Shelk", "Humanity", 15);
+
+const years = Teacher.combinedYearsOfExperience([teacher1, teacher2]);
+
+console.log(years);
+
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
-
 try {
   module.exports = Teacher;
 } catch {
